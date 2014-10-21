ebfed79c1d5f802909eda008ca7cfeeb1a9d63b1
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-10-21 01:41:54 -0300

    Add nginx_strict_ssl attr, and forced TLSv1.

--- cookbooks/webapp/attributes/default.rb
+++ cookbooks/webapp/attributes/default.rb
@@ -1,1 +1,4 @@
 default[:ostn][:webapp_dir] = "/home/deploy/ostn/current"
+# The :nginx_strict_ssl will disable support for older browsers like IE6.
+# See note here: https://dev.guardianproject.info/projects/ostel/wiki/Nginx
+default[:ostn][:nginx_strict_ssl] = true
