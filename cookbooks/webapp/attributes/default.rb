d2fd2affa20452fb3195f8eca23cdd60c5d7e108
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-10-24 06:51:43 -0300

    Remove strict_ssl option and always use strict. re: POODLE.

--- cookbooks/webapp/attributes/default.rb
+++ cookbooks/webapp/attributes/default.rb
@@ -1,5 +1,2 @@
 default[:ostn][:webapp_dir] = "/home/deploy/ostn/current"
 default[:ostn][:db_name] = "ostn_staging"
-# The :nginx_strict_ssl will disable support for older browsers like IE6.
-# See note here: https://dev.guardianproject.info/projects/ostel/wiki/Nginx
-default[:ostn][:nginx_strict_ssl] = true
